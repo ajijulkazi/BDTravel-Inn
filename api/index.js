3778b3f9931aceae29f696ba50fa7e2e438ab639
ajijul-kazi <ajijulkazi0178@gmail.com>
2023-02-19 04:26:39 +0600

    redirect from login to main index

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
  const mongoose = require('mongoose');
+ const bcrypt = require('bcryptjs');
+ const jwt = require('jsonwebtoken');
+ const User = require('./models/User.js'); 
+ 
 require('dotenv').config();
 
 const app = express();
 
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'gdialSKJAKSIIJASHGCSJA';
 
 app.use(express.json());
 
@@ -13,20 +19,49 @@ app.use(cors({
     origin: 'http://127.0.0.1:5173',
 }));
 
-// console.log(process.env.MONGO_URL);
+ //console.log(process.env.MONGO_URL);
 
- mongoose.set('strictQuery', true);
- mongoose.connect("mongodb+srv://booking:9Dy9D3GI2WRAGUX6@cluster0.tfzqfua.mongodb.net/?retryWrites=true&w=majority");
+  mongoose.set('strictQuery', true);
+ mongoose.connect(process.env.MONGO_URL);
  
 app.get('/test', (req, res) => {
     res.json('test ok');
 });
 // 9Dy9D3GI2WRAGUX6
 
-app.post('/register', (req, res) =>{
+app.post('/register',async (req, res) =>{
     const {name,email,password} = req.body;
-    res.json({name,email,password});
+   try {
+    const userDoc = await User.create({
+        name,
+        email,
+        password:bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+   } catch (e) {
+    res.status(422).json(e);
+   }
 }); 
 
+app.post('/login', async (req,res) =>{
+    const {email, password} = req.body;
+    const userDoc = await User.findOne({email});
+    if(userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if(passOk) {
+            jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token) =>{
+                if(err) throw err;
+                res.cookie('token', token).json('pass ok');
+            });
+            
+        } else {
+            res.json('pass not ok');
+        }
+        
+    } else {
+        res.status(422).json('not found');
+    }
+})
 
-app.listen(3001);
+
+app.listen(4000);
